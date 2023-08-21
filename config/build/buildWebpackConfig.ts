7ea8569df9e707800bdec9c9959d2fcb6b011c3f
gapur <kaloycant@gmail.com>
2023-08-21 16:32:07 +0200

    initial_setup: webpack dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,11 +1,12 @@
 import webpack from "webpack";
+import { buildDevServer } from "./buildDevServer";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
 
   return {
     mode, // 'production' or 'development'
@@ -20,5 +21,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map' : undefined, // Emit a source map for easier debugging
+    devServer: isDev ? buildDevServer(options) : undefined, // Dev server configuration
   }
 }

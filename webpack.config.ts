d8113d05bf830da9bd0e16585065a8f8bd5c1acb
gapur <kaloycant@gmail.com>
2023-08-21 16:46:59 +0200

    initial_setup: env setup and scripts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,22 +1,26 @@
 import path from "path";
 import webpack from "webpack";
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildEnv } from "./config/build/types/config";
 
-const paths = {
-  entry: path.resolve(__dirname, "src", "index.ts"),
-  build: path.resolve(__dirname, "build"),
-  htmlTemplate: path.resolve(__dirname, "public", "index.html"),
-}
+export default (env: BuildEnv) => {
+  const paths = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    build: path.resolve(__dirname, "build"),
+    htmlTemplate: path.resolve(__dirname, "public", "index.html"),
+  }
 
-const mode = "development";
-const isDev = mode === "development";
-const PORT = 3000;
+  const PORT = env.port || 3000;
+  const mode = env.mode || "development";
 
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: "development",
-  paths,
-  isDev,
-  port: PORT,
-});
+  const isDev = mode === "development";
 
-export default config;
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+
+  return config;
+};

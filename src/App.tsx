ddd5c90107730f69fb4b4e0c1a4949ed09ad9f76
gapur <kaloycant@gmail.com>
2023-08-21 19:58:27 +0200

    initial_setup: Routing, Code splitting, Lazy, Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Counter } from "./components/Counter/Counter";
-import { FC } from "react";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Link, Route, Routes } from "react-router-dom";
+import { FC, Suspense } from "react";
 
 const App: FC = () => {
   return (
-    <Counter />
+    <>
+      <div className="app">
+        <Link to={"/"}>Main</Link>
+        <Link to={"/about"}>About</Link>
+        <Suspense fallback={<div>loading...</div>}>
+          <Routes>
+            <Route path="/" element={<MainPageAsync/>}/>
+            <Route path="/about" element={<AboutPageAsync/>}/>
+          </Routes>
+        </Suspense>
+      </div>
+    </>
   )
 }
 
